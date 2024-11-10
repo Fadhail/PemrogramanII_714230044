75babbc916ebf7be046e56e89554a5cddba2b2f6
Fadhail <mochammadfadhail2@gmail.com>
2024-11-10 14:13:33 +0700

    Praktikum week 5

---
+++ Pertemuan-5/Praktikum/P5_3_714230044/P5_3_714230044/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace P5_3_714230044
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MerkHPLabel = new System.Windows.Forms.Label();
+            this.OSLabel = new System.Windows.Forms.Label();
+            this.SudahDiperbaiki = new System.Windows.Forms.Label();
+            this.txtMerkHP = new System.Windows.Forms.TextBox();
+            this.rb_android = new System.Windows.Forms.RadioButton();
+            this.rb_ios = new System.Windows.Forms.RadioButton();
+            this.cbYa = new System.Windows.Forms.CheckBox();
+            this.btnTampilkan = new System.Windows.Forms.Button();
+            this.btnKeluar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MerkHPLabel
+            // 
+            this.MerkHPLabel.AutoSize = true;
+            this.MerkHPLabel.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MerkHPLabel.Location = new System.Drawing.Point(34, 34);
+            this.MerkHPLabel.Name = "MerkHPLabel";
+            this.MerkHPLabel.Size = new System.Drawing.Size(68, 20);
+            this.MerkHPLabel.TabIndex = 0;
+            this.MerkHPLabel.Text = "Merk HP";
+            // 
+            // OSLabel
+            // 
+            this.OSLabel.AutoSize = true;
+            this.OSLabel.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OSLabel.Location = new System.Drawing.Point(34, 89);
+            this.OSLabel.Name = "OSLabel";
+            this.OSLabel.Size = new System.Drawing.Size(28, 20);
+            this.OSLabel.TabIndex = 2;
+            this.OSLabel.Text = "OS";
+            // 
+            // SudahDiperbaiki
+            // 
+            this.SudahDiperbaiki.AutoSize = true;
+            this.SudahDiperbaiki.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SudahDiperbaiki.Location = new System.Drawing.Point(34, 141);
+            this.SudahDiperbaiki.Name = "SudahDiperbaiki";
+            this.SudahDiperbaiki.Size = new System.Drawing.Size(135, 20);
+            this.SudahDiperbaiki.TabIndex = 3;
+            this.SudahDiperbaiki.Text = "Sudah Diperbaiki?";
+            // 
+            // txtMerkHP
+            // 
+            this.txtMerkHP.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMerkHP.Location = new System.Drawing.Point(198, 34);
+            this.txtMerkHP.Name = "txtMerkHP";
+            this.txtMerkHP.Size = new System.Drawing.Size(200, 23);
+            this.txtMerkHP.TabIndex = 4;
+            // 
+            // rb_android
+            // 
+            this.rb_android.AutoSize = true;
+            this.rb_android.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_android.Location = new System.Drawing.Point(202, 89);
+            this.rb_android.Name = "rb_android";
+            this.rb_android.Size = new System.Drawing.Size(74, 22);
+            this.rb_android.TabIndex = 5;
+            this.rb_android.TabStop = true;
+            this.rb_android.Text = "Android";
+            this.rb_android.UseVisualStyleBackColor = true;
+            // 
+            // rb_ios
+            // 
+            this.rb_ios.AutoSize = true;
+            this.rb_ios.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_ios.Location = new System.Drawing.Point(313, 89);
+            this.rb_ios.Name = "rb_ios";
+            this.rb_ios.Size = new System.Drawing.Size(46, 22);
+            this.rb_ios.TabIndex = 6;
+            this.rb_ios.TabStop = true;
+            this.rb_ios.Text = "IOS";
+            this.rb_ios.UseVisualStyleBackColor = true;
+            // 
+            // cbYa
+            // 
+            this.cbYa.AutoSize = true;
+            this.cbYa.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbYa.Location = new System.Drawing.Point(203, 142);
+            this.cbYa.Name = "cbYa";
+            this.cbYa.Size = new System.Drawing.Size(43, 22);
+            this.cbYa.TabIndex = 7;
+            this.cbYa.Text = "Ya";
+            this.cbYa.UseVisualStyleBackColor = true;
+            // 
+            // btnTampilkan
+            // 
+            this.btnTampilkan.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTampilkan.Location = new System.Drawing.Point(97, 210);
+            this.btnTampilkan.Name = "btnTampilkan";
+            this.btnTampilkan.Size = new System.Drawing.Size(90, 25);
+            this.btnTampilkan.TabIndex = 8;
+            this.btnTampilkan.Text = "Tampilkan";
+            this.btnTampilkan.UseVisualStyleBackColor = true;
+            this.btnTampilkan.Click += new System.EventHandler(this.btnTampilkan_Click);
+            // 
+            // btnKeluar
+            // 
+            this.btnKeluar.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnKeluar.Location = new System.Drawing.Point(264, 210);
+            this.btnKeluar.Name = "btnKeluar";
+            this.btnKeluar.Size = new System.Drawing.Size(90, 25);
+            this.btnKeluar.TabIndex = 9;
+            this.btnKeluar.Text = "Keluar";
+            this.btnKeluar.UseVisualStyleBackColor = true;
+            this.btnKeluar.Click += new System.EventHandler(this.btnKeluar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(450, 276);
+            this.Controls.Add(this.btnKeluar);
+            this.Controls.Add(this.btnTampilkan);
+            this.Controls.Add(this.cbYa);
+            this.Controls.Add(this.rb_ios);
+            this.Controls.Add(this.rb_android);
+            this.Controls.Add(this.txtMerkHP);
+            this.Controls.Add(this.SudahDiperbaiki);
+            this.Controls.Add(this.OSLabel);
+            this.Controls.Add(this.MerkHPLabel);
+            this.Name = "Form1";
+            this.Text = "Service HP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MerkHPLabel;
+        private System.Windows.Forms.Label OSLabel;
+        private System.Windows.Forms.Label SudahDiperbaiki;
+        private System.Windows.Forms.TextBox txtMerkHP;
+        private System.Windows.Forms.RadioButton rb_android;
+        private System.Windows.Forms.RadioButton rb_ios;
+        private System.Windows.Forms.CheckBox cbYa;
+        private System.Windows.Forms.Button btnTampilkan;
+        private System.Windows.Forms.Button btnKeluar;
+    }
+}
+
