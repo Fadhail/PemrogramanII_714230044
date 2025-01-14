2905832765de60cfd0f33924dddd63316a08fe62
Fadhail <mochammadfadhail2@gmail.com>
2025-01-14 07:29:56 +0000

    week 13

---
+++ Pertemuan13/Praktikum/P13_714230044/P13_714230044/view/StartUp.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace P13_714230044.view
+{
+    partial class StartUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartUp));
+            labelProject = new Label();
+            progressBar1 = new ProgressBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // labelProject
+            // 
+            labelProject.AutoSize = true;
+            labelProject.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelProject.Location = new Point(271, 304);
+            labelProject.Name = "labelProject";
+            labelProject.Size = new Size(291, 29);
+            labelProject.TabIndex = 0;
+            labelProject.Text = "PROJECT APLICATION";
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(12, 386);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(776, 39);
+            progressBar1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Enabled = false;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(162, 75);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(495, 191);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // StartUp
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            ControlBox = false;
+            Controls.Add(pictureBox1);
+            Controls.Add(progressBar1);
+            Controls.Add(labelProject);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "StartUp";
+            StartPosition = FormStartPosition.CenterScreen;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelProject;
+        private ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox1;
+    }
+}
